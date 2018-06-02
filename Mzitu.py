203f8d6334cb7f74355055787139160e37229e61
Zhaowei <linglangley@outlook.com>
2018-06-03 01:22:39 +0800

    v1.1.2

--- Mzitu.py
+++ Mzitu.py
@@ -1,4 +1,4 @@
-# coding:utf-8
+# encoding:utf-8
 
 import os
 import requests
@@ -13,9 +13,6 @@ x = int(input('客官要第几页？\n'))
 y = int(input('欲望的尽头是？\n'))
 path = 'E:/Mzitu/'
 Errorlist = []
-
-
-# http://www.mzitu.com/136673
 
 
 def getHtml(url, code='utf-8', id2=id):
@@ -36,9 +33,10 @@ def getList(x, y, tplt):
     cnt = 0
     try:
         with open(path2, 'a')as log:
+            log.write('\n新的开始\n')
             for link in Page_links:
                 cnt = cnt + 1
-                log.write('\n第{:4}页\n'.format(cnt))
+                log.write('\n第{:^4}页\n'.format(cnt))
                 r = getHtml(link)
                 soup = BeautifulSoup(r.text, 'lxml')
                 urls = soup.find('ul', attrs={'id': 'pins'})
@@ -62,53 +60,70 @@ def getList(x, y, tplt):
 
 def Htmlparser(lists, olist):
     try:
-        for url in lists:
-            r = getHtml(url).text
-            soup = BeautifulSoup(r, 'lxml')
-            s = soup.find('div', attrs={'class': 'pagenavi'}).find_all('span')[-2].string
-            s = int(s)
-            mainim = soup.find('div', attrs={'class': 'main-image'})
-            getImage(url, s, mainim)
-            olist.append(url)
-            sleep(random.randint(1, 5))
-            print('/r 已完成{:^6.4}%'.format(100 * len(olist) / len(lists)), end='')
+        with open(path + 'ErrorLog.txt', 'a') as comlog:  # 错误文档记录
+            comlog.write('\nA new list:\n')
+            for url in lists:
+                r = getHtml(url).text
+                soup = BeautifulSoup(r, 'lxml')
+                s = soup.find('div', attrs={'class': 'pagenavi'}).find_all('span')[-2].string
+                s = int(s)
+                mainim = soup.find('div', attrs={'class': 'main-image'})
+                x = getImage(url, s, mainim)
+                if x == 11:
+                    sleep(random.randint(1, 5))
+                else:
+                    cnte = 0
+                    for i in Errorlist:
+                        cnte = cnte + 1
+                        comlog.write('{:^3}\t{}\t{}\n'.format(cnte, url, x))
+                olist.append(url)
+                print('\r已完成{:^6.4}%'.format(100 * len(olist) / len(lists)), end='')
+        comlog.close()
     except:  # Exception as b:
         print('')
 
 
-def makedir(s):
-    f = path + s
+def makedir(f):
     isexisted = os.path.exists(f)
     if not isexisted:
         os.mkdir(f)
-    return f
+        return 1
+    else:
+        return 0
 
 
 def getImage(url, s, mainim):
     try:
         im = mainim.find('img')  # 是名称和地址
-        path1 = makedir(im.attrs['alt'])  # 套图文件夹
-        pic = im.attrs['src']
-        if s >= 100:
-            print(url)
-            pic_url = pic[0:-7]
-            qqq = 3
+        f = (path + (im.attrs['alt']).replace(":", " ")).replace("?", " ")
+        sign = makedir(f)  # 套图文件夹
+        if sign:
+            pic = im.attrs['src']
+            if s >= 100:
+                print(url)
+                pic_url = pic[0:-7]
+                qqq = 3
+            else:
+                pic_url = pic[0:-6]
+                qqq = 2
+            img0 = pic_url + '01' + '.jpg'
+            img_origin = getHtml(img0, id2=dict(id, **{'Referer': pic}))
+            if img_origin == 23:
+                Errorlist.append(url)
+                getImage2(mainim, s, f)  # 第二种路线
+                return 11
+            else:
+                savepic(f, '01', img_origin)
+                for i in range(2, s + 1):
+                    no = str(i).rjust(qqq, '0')
+                    img0 = pic_url + no + '.jpg'
+                    img = getHtml(img0, id2=dict(id, **{'Referer': pic}))
+                    savepic(f, no, img)
+                    sleep(1)
+                return 11
         else:
-            pic_url = pic[0:-6]
-            qqq = 2
-        img0 = pic_url + '01' + '.jpg'
-        img_origin = getHtml(img0, id2=dict(id, **{'Referer': pic}))
-        if img_origin == 23:
-            Errorlist.append(url)
-            getImage2(mainim, s, path1)  # 第二种路线
-        else:
-            savepic(path1, '01', img_origin)
-            for i in range(2, s + 1):
-                no = str(i).rjust(qqq, '0')
-                img0 = pic_url + no + '.jpg'
-                img = getHtml(img0, id2=dict(id, **{'Referer': pic}))
-                savepic(path1, no, img)
-                sleep(1)
+            print('\tSaved ever')
+            return f
     except Exception as c:
         print(c)
 
@@ -134,17 +149,11 @@ def savepic(path1, b, img):
 
 def main():
     # pat = re.compile('\/|\.')
-    makedir('')
+    makedir(path[0:-1])
     olist = []
     tplt = '{:^4}\t{}\n'
     img_urls = getList(x, y, tplt)
     Htmlparser(img_urls, olist)
-    with open(path + 'ErrorLog.txt', 'a') as comlog:  # 错误文档记录
-        cnte = 0
-        for i in olist:
-            cnte = cnte + 1
-            comlog.write(tplt.format(cnte, i))
-        comlog.close()
     with open(path + 'Completed log.txt', 'a') as comlog:
         cnt = 0
         for i in olist:
